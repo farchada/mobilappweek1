ca91250cff8e2249b9f6fc2f28cead619cc26ddb
farchada <farchada06@gmail.com>
2021-09-23 17:42:46 +0700

    datebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,28 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:mobileappweek1/config/constant.dart';
+import 'package:mobileappweek1/screen/dashboard.dart';
 import 'package:mobileappweek1/screen/home.dart';
 import 'package:mobileappweek1/screen/index.dart';
 import 'package:mobileappweek1/screen/login.dart';
 import 'package:mobileappweek1/screen/register.dart';
 
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-
   @override
-Widget build(BuildContext context){
-  return MaterialApp(
-    
-    home: Index(),
-    routes: {
-      'Login' : (context) => Login(),
-       'register' : (context) => Register(),
-    },
-    theme: ThemeData(
-      primaryColor: sColor,
-      secondaryHeaderColor: tColor,
-    ),
-  );
-
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Index(),
+      routes: {
+        'Login': (context) => Login(),
+        'register': (context) => Register(),
+        'dashboard': (context) => Dashboard(),
+      },
+      theme: ThemeData(
+        primaryColor: sColor,
+        secondaryHeaderColor: tColor,
+      ),
+    );
   }
 }
--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobileappweek1/backend/database.dart';
 import 'package:mobileappweek1/config/constant.dart';
 
 class Register extends StatefulWidget {
@@ -177,17 +178,18 @@ class _RegisterState extends State<Register> {
 
   Widget btnSubmit() => ElevatedButton(
         style: ElevatedButton.styleFrom(
-          primary: tColor,
+          primary: pColor,
         ),
         onPressed: () {
-          print('Hello World');
+          print("Hello");
+
+          var local = new DBLocal();
+
           if (formKey.currentState!.validate()) {
             formKey.currentState!.save();
-            print('Name : $name');
-            print('Surname : $surname');
-            print('Email : $email');
-            print('Password : $password');
+            local.register(name, surname, email, password);
             formKey.currentState!.reset();
+            Navigator.pushNamed(context, 'Login');
           }
         },
         child: Text('Submit'),
