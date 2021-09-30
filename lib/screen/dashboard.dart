a6fe696d8e1c9a772e9a7f3fd20d816035c2eec1
Chadaporn <farchada06@gmail.com>
2021-09-30 17:39:32 +0700

    api

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobileappweek1/config/constant.dart';
+import 'package:mobileappweek1/model/tct.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -8,10 +11,83 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  var data;
+
+  @override
+  void initState() {
+    super.initState();
+    print("Hello");
+    callAPI();
+  }
+
+  Future<void> callAPI() async {
+    var url = Uri.parse("https://www.boredapi.com/api/activity");
+
+    var response = await http.get(url);
+
+    setState(() {
+      data = tctFromJson(response.body);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Dashboard'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.api),
+            SizedBox(width: 10),
+            Text('Dashboard'),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Image(
+              image: NetworkImage(
+                  "https://images.unsplash.com/photo-1580926056910-fc3020ca1448?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"),
+              alignment: Alignment.center,
+            ),
+            Text(
+              data?.activity ?? "loading..",
+              style: TextStyle(
+                color: tColor,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              data?.type ?? '',
+              style: TextStyle(
+                color: pColor,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '${data?.price ?? ""}',
+              style: TextStyle(
+                color: sColor,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '${data?.participants ?? ""}',
+              style: TextStyle(
+                color: pColor,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
